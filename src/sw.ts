bb33da5774f72fe2e5c7240f757b92050b1fd9f8
Natasha <92600072+TXSHGRXY@users.noreply.github.com>
2026-06-05 19:22:49 +0100

    Initial Commit

--- src/sw.ts
+++ src/sw.ts
@@ -2,6 +2,8 @@
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
 
+declare let self: ServiceWorkerGlobalScope
+
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING')
     self.skipWaiting()
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
+  plugins: [react(), VitePWA({
     strategies: 'injectManifest',
     srcDir: 'src',
-    filename: 'sw.js',
+    filename: 'sw.ts',
     registerType: 'prompt',
     injectRegister: false,
 
@@ -17,9 +17,9 @@ export default defineConfig({
     },
 
     manifest: {
-      name: 'virtualgreenhouse',
-      short_name: 'virtualgreenhouse',
-      description: 'An app to track and manage your plants',
+      name: 'VirtualGreenhouse',
+      short_name: 'VirtualGreenhouse',
+      description: 'A webapp for you to track and monitor your plants',
       theme_color: '#ffffff',
     },
 
